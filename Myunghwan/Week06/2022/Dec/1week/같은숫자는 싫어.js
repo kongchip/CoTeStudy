e9f286af84b9095e38628976bec8f8d2141c63f4
[firstname lastname] <[valid-email]>
2022-12-09 09:25:37 +0900

    Level 1 / 20m 58s

--- Myunghwan/Week06/2022/Dec/1week/같은숫자는 싫어.js
+++ Myunghwan/Week06/2022/Dec/1week/같은숫자는 싫어.js
@@ -1,9 +1,9 @@
 function solution(arr)
 {
-    let answer = [];
+    let result = [];
     for(let i = 0; i < arr.length; i++){
         if(arr[i] !== arr[i+1])
-        answer.push(arr[i])
+        result.push(arr[i])
     }
-    return answer;
+    return result;
 }
