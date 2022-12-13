58818627416007084202f73077260c351664c5f0
[firstname lastname] <[valid-email]>
2022-12-13 20:54:48 +0900

    Level 1 / 1h 5m

---
+++ Myunghwan1/Myunghwan/2022/Dec/1week/시저 암호.js
@@ -0,0 +1,28 @@
+function solution(s, n) {
+    // 유니코드로 바꾸는 매소드 charCodeAt() // A: 65 | Z: 90 | a: 97 | z: 122 
+    // 유니코드를 문자열로 바꾸는 매소드 String.fromCharCode()
+    let answer = ''
+    let s_unicode = 0;
+//     let s_split = s.split('')
+    
+//     for(let i = 0; i < s_split.length; i++){
+//         s_unicode = s_split[i].charCodeAt();
+//         if(s_unicode === 32){
+//           s_unicode = s_unicode - n
+//         } else if(s_unicode === 122){
+//             s_unicode = s_unicode - 26
+//         }
+//         answer += String.fromCharCode(s_unicode+n)
+//     }
+//     return answer
+    return s.split("").map((el)=>{
+        if (el == " "){
+            return el;
+        }
+        const tmp = el.charCodeAt()
+        return el.toUpperCase().charCodeAt()+n > 90 
+            ? String.fromCharCode(tmp+n-26) 
+            : String.fromCharCode(tmp+n)
+    }).join('')
+
+}
