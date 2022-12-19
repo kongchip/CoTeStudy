55bf6c330f788dff196b971a8429d521bc75ca16
[firstname lastname] <[valid-email]>
2022-12-19 12:30:32 +0900

    Level 1 / 20m

--- Myunghwan/2022/Dec/week3/01. 2016년.js
+++ Myunghwan/2022/Dec/week3/01. 2016년.js
@@ -1,4 +1,16 @@
 function solution(a, b) {
     let date = new Date(2016, a-1, b);
+
+    // console.log(date) : 2016-05-23T15:00:00.000Z
+
+    // console.log(`${date}`) : Tue May 24 2016 00:00:00 GMT+0900 (Korean Standard Time)
+    
+//     console.log(`${date}`.split(' ')) : [
+//   'Tue',      'May',
+//   '24',       '2016',
+//   '00:00:00', 'GMT+0900',
+//   '(Korean',  'Standard',
+//   'Time)'
+// ]
     return `${date}`.split(' ')[0].toUpperCase()
 }
