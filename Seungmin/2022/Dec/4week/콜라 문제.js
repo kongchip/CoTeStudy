21e521192c8c933d21ee7a882718f3f5dc49a9d5
seungmin2ee <tmdals2815@gmail.com>
2022-12-28 02:10:17 +0900

    level1 / 40min

---
+++ Seungmin/2022/Dec/4week/콜라 문제.js
@@ -0,0 +1,21 @@
+// 빈병 a개 당 콜라 b병
+// 가지고 있는 빈병 수 n
+// 한번 교환으로 받을 수 있는 콜라 수 = n/a*b
+function solution(a, b, n) {
+    let result = 0;
+    let totalBottle = n;
+    let rest = 0;
+    // n이 a보다 크거나 같을때 까지 반복문
+    while(totalBottle >= a){
+        if(totalBottle%a === 0){
+            result += totalBottle/a*b;
+        } else {
+            result += Math.floor(totalBottle/a)*b;
+            rest = totalBottle % a;
+        }
+        totalBottle = Math.floor(totalBottle/a)*b + rest;
+        rest = 0;
+
+    }
+    return result;
+}
