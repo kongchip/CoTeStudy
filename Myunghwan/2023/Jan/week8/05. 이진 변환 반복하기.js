1820a314814efb54a1bdba1d326a1e879bed2a63
kongchip <myunghwan0321@gmail.com>
2023-02-01 18:53:51 +0900

    Level2 / 1h + 참고

--- Myunghwan/2023/Jan/week8/05. 이진 변환 반복하기.js
+++ Myunghwan/2023/Jan/week8/05. 이진 변환 반복하기.js
@@ -1,6 +1,13 @@
-function solution(A,B){
-    const Asort = A.sort((a,b) => a-b)
-    const Bsort = B.sort((a,b) => b-a)
-    
-    return Asort.reduce((acc, cur, index) => acc += cur*Bsort[index], 0);
+function solution(s) {
+    let deleteZero =0
+    let count =0
+    while(s.length!==1){
+        const originLen = s.length
+        s=s.split('').filter(v=>v==='1').join('')
+        const len = s.length
+        deleteZero+=originLen-len
+        s=len.toString(2)
+        count++
+    }
+    return[count,deleteZero]
 }
