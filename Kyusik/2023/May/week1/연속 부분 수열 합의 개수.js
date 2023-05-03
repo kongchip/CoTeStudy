6e4e8db7d8f6adda7da31f36895106c97e3532c3
starplanter93 <starplanter93@gmail.com>
2023-05-03 17:44:42 +0900

    level2 / 40m

---
+++ Kyusik/2023/May/week1/연속 부분 수열 합의 개수.js
@@ -0,0 +1,14 @@
+function solution(elements) {
+  // 중복되는 값을 지워야하므로 set 사용
+  const resultSet = new Set();
+
+  for (let i = 0; i < elements.length; i++) {
+    const newArr = elements.concat(elements.slice(0, i));
+
+    for (let j = 0; j < elements.length + i; j++) {
+      resultSet.add(newArr.slice(j, j + i).reduce((acc, cur) => acc + cur, 0));
+    }
+  }
+
+  return resultSet.size;
+}
