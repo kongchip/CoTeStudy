da34414ed1a5f807e138a493c6bc72466f557903
kongchip <myunghwan0321@gmail.com>
2023-08-06 02:59:14 +0900

    30m

--- Myunghwan/2023/Aug/2차원 배열/b2738.js
+++ Myunghwan/2023/Aug/2차원 배열/b2738.js
@@ -1,3 +1,13 @@
+/* 입력
+3 3
+1 1 1
+2 2 2
+0 1 0
+3 3 3
+4 4 4
+5 5 100
+*/
+
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
