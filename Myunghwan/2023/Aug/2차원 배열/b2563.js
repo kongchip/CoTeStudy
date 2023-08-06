1512869a11b95f5173d86312a522506bdeb22a8b
kongchip <myunghwan0321@gmail.com>
2023-08-06 20:38:14 +0900

    색종이 / 1h+

--- Myunghwan/2023/Aug/2차원 배열/b2563.js
+++ Myunghwan/2023/Aug/2차원 배열/b2563.js
@@ -1,12 +1,34 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().split('\n');
+// const filePath = `linux` === process.platform ? `dev/stdin` : 'input.txt';
+// const lines = require('fs').readFileSync(filePath).toString().trim().split(`\n`);
+
+// let drawingPaper = Array.from(Array(100), () => Array(100).fill(0));
+// const confetti = lines.slice(1).map(line => line.split(` `).map(Number));
+// let size = 0;
+
+// if (Number(lines[0]) !== confetti.length) return;
+
+// confetti.forEach(line => {
+//     const [x, y] = line;
+
+//     for(let i = x; i < x+10; i++) {
+//         for(let j = y; j < y+10; j++) {
+//             if(1 === drawingPaper[i][j]) continue;
+//             drawingPaper[i][j] = 1;
+//             size++;
+//         }
+//     }
+// })
+
+// console.log(size)
+
+const filePath = `linux` === process.platform ? `dev/stdin` : 'input.txt';
+const lines = require('fs').readFileSync(filePath).toString().trim().split(`\n`);
 
 let drawingPaper = Array.from(Array(100), () => Array(100).fill(0));
-const confetti = input.slice(1).map((line) => line.split(` `).map(Number));
+const confetti = lines.slice(1).map((line) => line.split(` `).map(Number));
 let size = 0;
 
-if (Number(input[0]) !== confetti.length) return;
+if (Number(lines[0]) !== confetti.length) return;
 
 confetti.forEach((line) => {
   const [x, y] = line;
