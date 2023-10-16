2255fb33838a49676338eb0a8accd479831bea7c
seungmin2ee <tmdals2815@gmail.com>
2023-10-17 00:05:34 +0900

    [백준] 회사에 있는 사람 / 리팩토링

--- Seungmin/2023/Oct/3week/회사에 있는 사람.js
+++ Seungmin/2023/Oct/3week/회사에 있는 사람.js
@@ -2,26 +2,44 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-// solution1 => 시간초과
 const solution = () => {
-  let result = ''
-  let list = {
-    'enter': [],
-    'leave': [],
-  }
+  let setObj = new Set()
 
   input.shift()
 
   let arr = input.map(el => el.split(' '))
 
-  // list 객체에 추가
-  arr.forEach(el => el.includes('enter') ? list.enter.push(el[0]) : list.leave.push(el[0]))
+  arr.forEach(el => {
+    const [name, action] = el
+
+    action === 'enter' ? setObj.add(name) : setObj.delete(name)
+  })
   
-  let filteredArr = list.enter.filter(el => !list.leave.includes(el))
-
-  filteredArr.sort((a, b) => b.localeCompare(a)).forEach(el => result += `${el}\n`)
-
-  console.log(result)
+  let answer = [...setObj].sort().reverse()
+  
+  console.log(answer.join('\n'))
 }
+
+// 시간초과
+// const solution = () => {
+//   let result = ''
+//   let list = {
+//     'enter': [],
+//     'leave': [],
+//   }
+
+//   input.shift()
+
+//   let arr = input.map(el => el.split(' '))
+
+//   // list 객체에 추가
+//   arr.forEach(el => el.includes('enter') ? list.enter.push(el[0]) : list.leave.push(el[0]))
+  
+//   let filteredArr = list.enter.filter(el => !list.leave.includes(el))
+
+//   filteredArr.sort((a, b) => b.localeCompare(a)).forEach(el => result += `${el}\n`)
+
+//   console.log(result)
+// }
 
 solution()
