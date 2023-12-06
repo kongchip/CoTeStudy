024c85664223fe0203efb3a7b5906bf7397828ed
seungmin2ee <tmdals2815@gmail.com>
2023-12-06 19:58:13 +0900

    [백준] 도키도키 간식드리미  / -

---
+++ Seungmin/2023/Dec/2week/도키도키 간식드리미.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+
+const solution = () => {  
+  let result
+
+  // 죄송해요... 못풀었어요...ㅠㅠㅠㅠㅠㅠㅠㅠ
+
+  console.log(result)
+}
+
+solution()
