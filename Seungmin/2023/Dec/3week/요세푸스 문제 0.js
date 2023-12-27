296da96a1f90d6bd1425cd9586a939663cd734bb
seungmin2ee <tmdals2815@gmail.com>
2023-12-27 17:18:42 +0900

    [백준] 요세푸스 문제 0  / 30min

---
+++ Seungmin/2023/Dec/3week/요세푸스 문제 0.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split(' ').map(Number);
+
+class ListNode {
+  constructor(item) {
+    this.item = item
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.size = 0
+  }
+
+  push(item) {
+    let node = new ListNode(item)
+
+    if(!this.head) {
+      this.head = node
+      this.head.next = this.tail
+    } else {
+      this.tail.next = node
+    }
+
+    this.tail = node
+    this.size++
+  }
+
+  getSize() {
+    return this.size
+  }
+
+  pop() {
+    if(this.size > 2) {
+      let item = this.head.item
+      let newHead = this.head.next
+
+      this.head = newHead
+      this.size--
+
+      return item
+    } else if (this.size === 2) {
+      let item = this.head.item
+      let newHead = this.head.next
+
+      this.head = newHead
+      this.tail = newHead
+      this.size--
+
+      return item
+    } else if (this.size === 1) {
+      let item = this.head.item
+      let newHead = this.head.next
+
+      this.head = null
+      this.tail = null
+      this.size--
+
+      return item
+    } else {
+      return -1
+    }
+  }
+
+  empty() {
+    return this.size ? 0 : 1
+  }
+
+  getFirst() {
+    return this.head ? this.head.item : -1
+  }
+
+  getLast() {
+    return this.tail ? this.tail.item : -1
+  }
+}
+
+const solution = () => {
+  let result = []
+  let [N, K] = input
+  let queue = new LinkedList()
+
+  for(let i = 1; i <= N; i++) {
+    queue.push(i)
+  }
+
+  for(let i  = 0; queue.getSize() > 1; i++) {
+    for(let j = 1; j < K; j++ ) {
+      queue.push(queue.pop())
+    }
+
+    result.push(queue.pop())
+  }
+
+  result.push(queue.pop())
+
+  console.log(`<${result.join(', ')}>`)
+}
+
+solution()
