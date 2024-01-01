ba6e14ff1feaae7eeec2ceb96860029b31eb1d04
seungmin2ee <tmdals2815@gmail.com>
2024-01-01 23:25:32 +0900

    [백준] 베라의 패션 / 5min

---
+++ Seungmin/2024/Jan/1week/베라의 패션.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt'
+let input = parseInt(fs.readFileSync(filePath).toString().trim())
+
+const solution = () => {  
+  let result = (input * input) - input
+
+  console.log(result)
+}
+
+solution()
