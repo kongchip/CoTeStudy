af766b5e487889a41c47bba87e79e048da776bdd
kongchip <myunghwan0321@gmail.com>
2024-01-02 21:33:20 +0900

    베라의 패션 / 5m

---
+++ Myunghwan/2024/Jan/1week/베라의 패션.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+let input = fs.readFileSync(filePath).toString().trim();
+
+const N = Number(input);
+console.log(N * N - N);
