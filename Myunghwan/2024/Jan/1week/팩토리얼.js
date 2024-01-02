552b457c5092c62e7d22a3e486795b3ca6f031bd
kongchip <myunghwan0321@gmail.com>
2024-01-02 22:16:16 +0900

    이항 계수1 / 30m

---
+++ Myunghwan/2024/Jan/1week/팩토리얼.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+let input = fs.readFileSync(filePath).toString().trim();
+
+const N = Number(input);
+
+// 재귀
+
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(N));
