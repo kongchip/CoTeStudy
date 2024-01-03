5b9e150839324c7ef3e95bc98079c37a41adf03f
ethanJcoding <ethan.95j@gmail.com>
2024-01-03 14:51:21 +0900

    [백준] 녹색거탑 / 30m

---
+++ Junil/2024/Jan/Week01/녹색거탑.js
@@ -0,0 +1,10 @@
+let input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
+  )
+  .toString()
+  .trim();
+
+const N = Number(input);
+
+console.log(2 ** N);
