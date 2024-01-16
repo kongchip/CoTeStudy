f874ae4542649ddb86975feda9de3ae2dba483cc
seungmin2ee <tmdals2815@gmail.com>
2024-01-16 22:57:13 +0900

    [백준] 영단어 암기는 괴로워 / 1h

--- Seungmin/2024/Jan/2week/영단어 암기는 괴로워.js
+++ Seungmin/2024/Jan/2week/영단어 암기는 괴로워.js
@@ -3,12 +3,15 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/inp
 let input = fs.readFileSync(filePath).toString().trim().split('\n')
 
 const solution = () => {
-  let result, voca = []
+  let result, voca
   /** * key: 단어
    * * value: 단어가 나온 횟수
    */
   let map = new Map()
   let [N, M] = input.shift().split(' ').map(Number)
+
+  // 1.사전순 정렬 -> 2.단어 길이로 내림차순 정렬
+  input.sort().sort((a, b) => b.length - a.length)
 
   input.forEach(el => {
     // 단어 길이 안되면 처음부터 map에 포함하지 않음
@@ -19,19 +22,12 @@ const solution = () => {
     } else {
       map.set(el, 1)
     }
-
   })
 
   // map객체를 2차원 배열화
-  map.forEach((val, key) => voca.push([key, val]))
+  voca = [...map]
 
-  // 사전순 정렬
-  voca.sort()
-
-  // 단어길이 내림차순 정렬
-  voca.sort((a, b) => b[0].length - a[0].length)
-
-  // 자주 나오는 단어 내림차순 정렬
+  // 3.자주 나오는 단어 내림차순 정렬
   voca.sort((a, b) => b[1] - a[1])
 
   result = voca.map(el => el[0])
